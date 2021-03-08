bb8fd9e7b7439970f4accfe748424ed0229dd3e5
Inexorably <ssantillo@ucla.edu>
2021-03-07 23:41:30 -0800

    Further fundamental prototyping / step function

--- shinkiro/linkage.cpp
+++ shinkiro/linkage.cpp
@@ -5,7 +5,7 @@
 /**********************************************************Base functions (constructor etc)************************************************/
 
 //Default Link constructor, primarily useful for testing so we fill with some arbitrary values.
-shinkiro::Link::Link() : m_length(2), m_radius(1), m_mass(1), m_inertia(1), m_theta(EIGEN_PI/4), m_omega(1), m_alpha(1), m_forceX(1), m_forceY(-1), m_torque(1) {
+shinkiro::Link::Link() : m_length(2), m_radius(1), m_mass(1), m_inertiaCenter(1), m_theta(EIGEN_PI/4), m_omega(1), m_alpha(1), m_forceX(1), m_forceY(-1), m_torque(1) {
 	if (SHOW_DEBUG_OUTPUT) {
 		std::cout << "shinkiro::Link::Link(): Link constructed succesfully.\n";
 	}
@@ -52,7 +52,7 @@ double shinkiro::Link::f_stw2() const {
 //Use inverse dynamics to calculate the forces and moments for a three link planar bipedal / inverted pendulum model.
 //Assumes foot is massless and remains grounded for period of analysis.  Assumes that position, velocity, and acceleration values are known and defined in m_theta, m_omega, m_alpha.
 //Returns a vector of the forces and torques of order [Fx1; Fx2; Fy1; Fy2; T1; T2; Fx3; Fy3; T3].
-Eigen::VectorXd shinkiro::Linkage::f_inverseDynamics() {
+Eigen::VectorXd shinkiro::Linkage::f_inverseDynamics() const {
 	//Handle the cases for the current number of links.  Eventually generically handle linkages, but for now prototype around 3 link model.
 	if (m_links.size() != 3) {
 		std::cout << "Invalid linkage length\n";
@@ -78,13 +78,13 @@ Eigen::VectorXd shinkiro::Linkage::f_inverseDynamics() {
 	//Define the vector B, which is 9x1.
 	b << -m_links[0].m_mass * m_links[0].m_radius * (m_links[0].f_sta() + m_links[0].f_ctw2()),
 		m_links[0].m_mass* (m_links[0].m_radius * (m_links[0].f_cta() - m_links[0].f_stw2()) + shinkiro::g),
-		m_links[0].m_inertia* m_links[0].m_alpha,
+		m_links[0].m_inertiaCenter* m_links[0].m_alpha,
 		-m_links[1].m_mass * (m_links[0].m_length * (m_links[0].f_sta() + m_links[0].f_ctw2()) + m_links[1].m_radius * (m_links[1].f_sta() + m_links[1].f_ctw2())),
 		m_links[1].m_mass* (m_links[0].m_length * (m_links[0].f_cta() - m_links[0].f_stw2()) + m_links[1].m_radius * (m_links[1].f_cta() - m_links[1].f_stw2()) + shinkiro::g),
-		m_links[1].m_inertia* m_links[1].m_alpha,
+		m_links[1].m_inertiaCenter* m_links[1].m_alpha,
 		-m_links[2].m_mass * (m_links[0].m_length * (m_links[0].f_sta() + m_links[0].f_ctw2()) + m_links[1].m_length * (m_links[1].f_sta() + m_links[1].f_ctw2()) + m_links[2].m_radius * (m_links[2].f_sta() + m_links[2].f_ctw2())),
 		m_links[2].m_mass* (m_links[0].m_length * (m_links[0].f_cta() - m_links[0].f_stw2()) + m_links[1].m_length * (m_links[1].f_cta() - m_links[1].f_stw2()) + m_links[2].m_radius * (m_links[2].f_cta() - m_links[2].f_stw2()) + shinkiro::g),
-		m_links[2].m_inertia* m_links[2].m_alpha;
+		m_links[2].m_inertiaCenter* m_links[2].m_alpha;
 
 
 	//Find f = b\A.
@@ -94,7 +94,7 @@ Eigen::VectorXd shinkiro::Linkage::f_inverseDynamics() {
 //Use forward dynamics to find the angular accelerations resulting from some given forces and torques.
 //Assumes foot is massless and remains grounded for period of analysis.
 //Returns a vector of angular accelerations of order [alpha1; alpha2; alpha3].
-Eigen::VectorXd shinkiro::Linkage::f_forwardDynamics() {
+Eigen::VectorXd shinkiro::Linkage::f_forwardDynamicsFull() const {
 	//Handle the cases for the current number of links.  Eventually generically handle linkages, but for now prototype around 3 link model.
 	if (m_links.size() != 3) {
 		std::cout << "Invalid linkage length\n";
@@ -105,15 +105,15 @@ Eigen::VectorXd shinkiro::Linkage::f_forwardDynamics() {
 	//TODO: Adapt this function for Linkage with n links, as the moment equations are consistently the same.
 	double alpha1 = (m_links[0].m_torque - m_links[1].m_torque + m_links[0].m_forceX * m_links[0].m_radius * sin(m_links[0].m_theta) - m_links[0].m_forceY * m_links[0].m_radius * cos(m_links[0].m_theta)
 		+ m_links[1].m_forceX * (m_links[0].m_length - m_links[0].m_radius) * sin(m_links[0].m_theta) - m_links[1].m_forceY * (m_links[0].m_length - m_links[0].m_radius) * cos(m_links[0].m_theta))
-		/ m_links[0].m_inertia;
+		/ m_links[0].m_inertiaCenter;
 
 	double alpha2 = (m_links[1].m_torque - m_links[2].m_torque + m_links[1].m_forceX * m_links[1].m_radius * sin(m_links[1].m_theta) - m_links[1].m_forceY * m_links[1].m_radius * cos(m_links[1].m_theta)
 		+ m_links[2].m_forceX * (m_links[1].m_length - m_links[1].m_radius) * sin(m_links[1].m_theta) - m_links[2].m_forceY * (m_links[1].m_length - m_links[1].m_radius) * cos(m_links[1].m_theta))
-		/ m_links[1].m_inertia;
+		/ m_links[1].m_inertiaCenter;
 
 	//For the final link, the end forces are zero.
 	double alpha3 = (m_links[2].m_torque + m_links[2].m_forceX * m_links[2].m_radius * sin(m_links[2].m_theta) - m_links[2].m_forceY * m_links[2].m_radius * cos(m_links[2].m_theta))
-		/ m_links[2].m_inertia;
+		/ m_links[2].m_inertiaCenter;
 
 	/*
 	//Check alpha1 from the equation 1.
@@ -126,3 +126,66 @@ Eigen::VectorXd shinkiro::Linkage::f_forwardDynamics() {
 
 	return temp;
 }
+
+//Use forward dynamics to find the angular accelerations resulting from torques only.
+//Assumes foot is massless and remains grounded for period of analysis.
+//Returns a vector of angular accelerations of order [alpha1; alpha2; alpha3].
+Eigen::VectorXd shinkiro::Linkage::f_forwardDynamicsTorques(Eigen::VectorXd torques) const {
+	//Handle the cases for the current number of links.  Eventually generically handle linkages, but for now prototype around 3 link model.
+	if (m_links.size() != 3) {
+		std::cout << "Invalid linkage length\n";
+		return Eigen::VectorXd(3);
+	}
+
+	if (torques.size() != 3) {
+		std::cout << "Invalid torque vector length\n";
+		return Eigen::VectorXd(3);
+	}
+
+	//Find the angular accelerations from the moment balance equations, moment around bottom pin joint (link origin).
+	//TODO: Account for reaction moments from masses of other links
+	double alpha1 = (torques[0] - torques[1] - m_links[0].m_mass * shinkiro::g * m_links[0].m_radius) / m_links[0].m_inertiaOrigin;
+	double alpha2 = (torques[1] - torques[2] - m_links[1].m_mass * shinkiro::g * m_links[1].m_radius) / m_links[1].m_inertiaOrigin;
+	double alpha3 = (torques[2] - m_links[3].m_mass * shinkiro::g * m_links[3].m_radius) / m_links[2].m_inertiaOrigin;
+
+	//Store and return the angular accelerations.
+	Eigen::VectorXd temp(m_links.size());
+	temp << alpha1, alpha2, alpha3;
+
+	return temp;
+}
+
+//Returns a linkage stepped forward using some input torques.
+shinkiro::Linkage shinkiro::Linkage::f_stepForwardTorques(const double dt, Eigen::VectorXd torques) const {
+	//TODO
+	
+	return *this;
+}
+
+//Uses the angles, angular accelerations, and positions as input.
+//Returns the cartsian positions, velocities, and accelerations for the current state.
+/*
+Eigen::VectorXd shinkiro::Linkage::f_findCartesianMotionQuantities() const {
+	//Handle the cases for the current number of links.  Eventually generically handle linkages, but for now prototype around 3 link model.
+	if (m_links.size() != 3) {
+		std::cout << "Invalid linkage length\n";
+		return Eigen::VectorXd(9);
+	}
+
+	//Initialize variables.
+	double px1, px2, px3, py1, py2, py3, vx1, vx2, vx3, vy1, vy2, vy3, ax1, ax2, ax3, ay1, ay2, ay3;
+
+	//Link 1.
+	px1 = m_links[0].m_radius * cos(m_links[0].m_theta);
+	py1 = m_links[0].m_radius * sin(m_links[0].m_theta);
+	vx1 = -m_links[0].m_radius * sin(m_links[0].m_theta) * m_links[0].m_omega;
+	vy1 = m_links[0].m_radius * cos(m_links[0].m_theta) * m_links[0].m_omega;
+
+	//Store and return the angular accelerations.
+	Eigen::VectorXd temp(m_links.size());
+
+	std::cout << "warning f_findCartesianMotionQuantities()\n";
+
+	return temp;
+}
+*/
--- shinkiro/linkage.h
+++ shinkiro/linkage.h
@@ -13,9 +13,13 @@ namespace shinkiro {
 	//Class defining individual links of a linkage.  Assumes units are consistent.
 	class Link {
 		public:
+			//Default constructor, primarily useful for testing so we fill with some arbitrary values.
+			Link();
+
 			double m_length;		//Link length.
 			double m_radius;		//Distance along the link from the link origin to the center of mass.
-			double m_inertia;
+			double m_inertiaCenter;	//Inertia about center.
+			double m_inertiaOrigin;	//Inertia about link origin.
 			double m_mass;
 			double m_theta;			//Current angle in rad.
 			double m_omega;			//Current angular velocity in rad/s.
@@ -31,9 +35,6 @@ namespace shinkiro {
 			double f_sta() const;		//Returns sin(theta)*alpha.
 			double f_ctw2() const;		//Returns cos(theta)*omega^2.
 			double f_stw2() const;		//Returns sin(theta)*omega^2.
-
-			//Default constructor, primarily useful for testing so we fill with some arbitrary values.
-			Link();
 	};
 
 	class Linkage {
@@ -43,13 +44,27 @@ namespace shinkiro {
 			//Use inverse dynamics to calculate the forces and moments for a three link planar bipedal / inverted pendulum model.
 			//Assumes foot is massless and remains grounded for period of analysis.
 			//Returns a vector of the forces and torques of order [Fx1; Fx2; Fy1; Fy2; T1; T2; Fx3; Fy3; T3].
-			Eigen::VectorXd f_inverseDynamics();
+			Eigen::VectorXd f_inverseDynamics() const;
 
 			//Use forward dynamics to find the angular accelerations resulting from some given forces and torques.
 			//Assumes foot is massless and remains grounded for period of analysis.
 			//Returns a vector of angular accelerations of order [alpha1; alpha2; alpha3].
-			Eigen::VectorXd f_forwardDynamics();
+			Eigen::VectorXd f_forwardDynamicsFull() const;
 
+			//Use forward dynamics to find the angular accelerations resulting from torques only.
+			//Assumes foot is massless and remains grounded for period of analysis.
+			//Returns a vector of angular accelerations of order [alpha1; alpha2; alpha3].
+			Eigen::VectorXd f_forwardDynamicsTorques(Eigen::VectorXd torques) const;
+
+			//Returns a linkage stepped forward using some input torques.
+			Linkage f_stepForwardTorques(const double dt, Eigen::VectorXd torques) const;
+
+			//Uses the angles, angular accelerations, and positions as input.
+			//Returns the cartsian positions, velocities, and accelerations for the current state.
+			//[theta1 theta2 theta3 omega1 omega2 omega3 alpha1 alpha2 alpha3].
+			//Eigen::VectorXd f_findCartesianMotionQuantities() const;	//TODO: UNFINISHED
+
+			//TODO: Update angular accelerations function that uses dynamicsTorques().
 
 			//Default constructor, primarily useful for testing so we fill with some arbitrary values.  Defaults to 3 link linkage.
 			Linkage();
